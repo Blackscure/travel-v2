2c4b196a2eaad4408f36fd13d948de40a95f0cb1
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-05 09:17:50 +0300

    routes created

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TravelAgentController;
 
-
-
+// Existing Sanctum route
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Registration and Login routes
+Route::post('/register', [TravelAgentController::class, 'register']);
+Route::post('/login', [TravelAgentController::class, 'login']);
