e87cce18b573f4cd39475e3e3e6635f7a0f9c206
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-06 12:11:30 +0300

    register api created in api file

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TravelAgentController;
+use App\Http\Controllers\AuthController;
 
 // Existing Sanctum route
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -10,5 +10,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Registration and Login routes
-Route::post('/register', [TravelAgentController::class, 'register']);
-Route::post('/login', [TravelAgentController::class, 'login']);
+
+Route::post('/register', [AuthController::class, 'register']);
