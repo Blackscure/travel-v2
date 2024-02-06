6d5814dc42d4b53df7475577b2c4d65d93440af1
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-06 13:12:48 +0300

    some modifications made in tracel controller

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TravelAgentController;
 
 // Existing Sanctum route
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -10,7 +10,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Registration and Login routes
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
+Route::post('/register', [TravelAgentController::class, 'register']);
+Route::post('/login', [TravelAgentController::class, 'login']);
