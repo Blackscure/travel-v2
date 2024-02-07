17d20ec0f72d9d7f12c82f5672dbe73cdd5171cf
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-07 12:19:28 +0300

    update modifie in routes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('/accommodations', [AccommodationController::class, 'create_accommodation']);
 Route::get('/accommodations', [AccommodationController::class, 'index']);
 Route::get('/accommodations/{id}', [AccommodationController::class, 'show']);
-Route::put('/accommodations/{id}', [AccommodationController::class, 'update']);
+Route::put('/accommodations/{id}', [AccommodationController::class, 'update_accomodation']);
 Route::delete('/accommodations/{id}', [AccommodationController::class, 'destroy']);
 
 Route::apiResource('contracts', ContractController::class);
