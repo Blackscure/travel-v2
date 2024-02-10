5427187db0ceddefd09556d9a83b86c90cb38948
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-10 23:12:20 +0300

    cor added in api file for all routes

--- routes/api.php
+++ routes/api.php
@@ -8,30 +8,39 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContractController;
 
 // Existing Sanctum route
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'cors'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
-// Registration and Login routes
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::get('/users', [AuthController::class, 'getAllUsers']); // New route for getting all users
+Route::middleware(['auth:sanctum', 'cors'])->group(function () {
 
-Route::put('/users/{id}', [AuthController::class, 'updateUser']); // New route for updating a user
+            // Registration and Login routes
 
-Route::delete('/users/{id}', [AuthController::class, 'deleteUser']); // New route for deleting a user
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+        Route::get('/users', [AuthController::class, 'getAllUsers']); // New route for getting all users
 
-Route::post('/accommodations', [AccommodationController::class, 'create_accommodation']);
-Route::get('/accommodations', [AccommodationController::class, 'index']);
-Route::get('/accommodations/{id}', [AccommodationController::class, 'show']);
-Route::put('/accommodations/{id}', [AccommodationController::class, 'update_accomodation']);
-Route::delete('/accommodations/{id}', [AccommodationController::class, 'destroy']);
+        Route::put('/users/{id}', [AuthController::class, 'updateUser']); // New route for updating a user
 
-Route::get('/bookings', [BookingController::class, 'get_bookings']);          // Get all bookings
-Route::get('/bookings/{id}', [BookingController::class, 'show']);      // Get a specific booking
-Route::post('/create-bookings', [BookingController::class, 'create_booking']);          // Create a new booking
-Route::put('/bookings/{id}', [BookingController::class, 'update_booking']);     // Update a booking
-Route::delete('/bookings/{id}', [BookingController::class, 'destroy']); // Delete a booking
+        Route::delete('/users/{id}', [AuthController::class, 'deleteUser']); // New route for deleting a user
 
-Route::apiResource('contracts', ContractController::class);
+        Route::post('/accommodations', [AccommodationController::class, 'create_accommodation']);
+        Route::get('/accommodations', [AccommodationController::class, 'index']);
+        Route::get('/accommodations/{id}', [AccommodationController::class, 'show']);
+        Route::put('/accommodations/{id}', [AccommodationController::class, 'update_accomodation']);
+        Route::delete('/accommodations/{id}', [AccommodationController::class, 'destroy']);
+
+        Route::get('/bookings', [BookingController::class, 'get_bookings']);          // Get all bookings
+        Route::get('/bookings/{id}', [BookingController::class, 'show']);      // Get a specific booking
+        Route::post('/create-bookings', [BookingController::class, 'create_booking']);          // Create a new booking
+        Route::put('/bookings/{id}', [BookingController::class, 'update_booking']);     // Update a booking
+        Route::delete('/bookings/{id}', [BookingController::class, 'destroy']); // Delete a booking
+
+        Route::apiResource('contracts', ContractController::class);
+
+});
+
+
