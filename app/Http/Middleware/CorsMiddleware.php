36b4279a13df2f90a7e5ad7b7bb65f6c8c790ca4
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-02-10 22:59:42 +0300

    ccors allowed

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,18 +3,18 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-        return $next($request);
+        $response = $next($request);
+
+        // Allow specific domains or '*' for any domain
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        return $response;
     }
 }
